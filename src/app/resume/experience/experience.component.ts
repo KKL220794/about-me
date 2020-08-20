9345c53598c593255bae36899ae9d334a1f25c96
krishna lal <krishna.lal@synechron.com>
2020-08-21 01:42:40 +0530

    experience component added

--- src/app/resume/experience/experience.component.ts
+++ src/app/resume/experience/experience.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+declare var jQuery: any;
+
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
+
+  isCollapsibleOpened;
 
   constructor() { }
 
   ngOnInit() {
+    this.isCollapsibleOpened = false;
+  }
+
+  ngAfterViewInit() {
+    jQuery('.collapsible').collapsible() ;
   }
 
 }
